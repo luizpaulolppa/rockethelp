f8d49181b58b8652283be5d3060857b322994f24
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-07-19 16:07:43 -0300

    added button component with Native Base

---
+++ src/components/Input.tsx
@@ -0,0 +1,22 @@
+import { Input as NBInput, IInputProps } from "native-base";
+
+export default function Input({ ...rest }: IInputProps) {
+  return (
+    <NBInput
+      bg="gray.700"
+      h={14}
+      size="md"
+      borderWidth={0}
+      fontSize="md"
+      fontFamily="body"
+      color="white"
+      placeholderTextColor="gray.300"
+      _focus={{
+        borderWidth: 1,
+        borderColor: "green.500",
+        bg: "gray.700",
+      }}
+      {...rest}
+    />
+  );
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,13 +1,31 @@
-import { VStack, Heading } from "native-base";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
 import Logo from "../assets/logo_primary.svg";
+import Input from "../components/Input";
 
 export default function SignIn() {
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={
+          <Icon ml={4} as={<Envelope color={colors.gray[300]} />} />
+        }
+      />
+      <Input
+        secureTextEntry
+        placeholder="Senha"
+        InputLeftElement={
+          <Icon ml={4} as={<Key color={colors.gray[300]} />} />
+        }
+      />
     </VStack>
   );
 }
